e666df845f0ea1f8b44a93d71e0d2e363a165e70
shock <mjacobs21321@gmail.com>
2018-09-07 15:37:59 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
+var cards = [cardOne= "queen","queen", cardTwo = "king", "king"];
+	cards.push('cardOne');
+	cards.push('cardTwo')
+	cardsInPlay = [];
+
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1] ){
+			alert("You found a match!");
+	}
+	}else{
+		alert("Sorry, try again.");
+		};
+		
+console.log("User flipped " + " " + cardOne);
+console.log("User flipped " + " " + cardTwo);
