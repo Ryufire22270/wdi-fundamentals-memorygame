534311387400e76168b4b2a8f3707eb87a1ce909
shock <mjacobs21321@gmail.com>
2018-09-08 16:20:05 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,4 @@
+//variable array for each card's rank, suit, and image
 var cards = [
 	{
 	rank: "queen",
@@ -26,31 +27,66 @@ var cards = [
 
 var	cardsInPlay = [];
 
+//var checks for a match
 var checkForMatch = function() {
 
-	if(cardsInPlay[0] === cardsInPlay[1] ){
+//checks if a match had been made
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		
-		alert("You found a match!");
-	} else{
-		alert("Sorry, try again.");
-		};		
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	}		
 };
 
-var flipCard = function(cardID) {
-	console.log("User flipped " + cards[cardID].rank);
+//function for flipping a card over
+var flipCard = function() {
+	
+	//gets the data-id of a card and logs it
+	cardId = this.getAttribute('data-id');
+	//logs user flipped and card[cardId].rank value
+	console.log("User flipped " + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardID.rank]);
+	//pushes string and value of rank into cardInPlay array
+	cardsInPlay.push(cards[cardId].rank);
 
-	console.log(cards[cardID].cardImage);
+	//logs display of image and suit
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	console.log(cards[cardID].suit)
-
+	//displays the card's image 
+	this.setAttribute('src', cards[cardId].cardImage)
+	
+	//checks to see how many cards have already been played
 	if(cardsInPlay.length === 2) {
 
+	//calls checkForMatch	
 	checkForMatch();
 	
 	}
-};		
-	
-flipCard(0);
-flipCard(2);
+};
+
+//function that creates the board
+var createBoard = function() {
+	//for loop that goes though your cards array
+
+	for (var i = 0; i < cards.length; i++) {
+
+	//creates and img element to work with 	
+	var cardElement = document.createElement('img');
+
+	//sets attribute to display the card flipped
+	cardElement.setAttribute('src', "images/back.png")
+
+	cardElement.setAttribute('data-id', i)
+
+	//adds a click event listener to "wait" for a click in order to flip
+	cardElement.addEventListener('click', flipCard);
+
+	//appends the card to the board you have created
+	document.getElementById('game-board').appendChild(cardElement);	
+	}
+};
+
+//calls the createBoard function so it can work
+createBoard();
